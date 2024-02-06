786d3b28887daabb6383ad84f22b8b102553bfa9
Jai <130995905+Jai1911@users.noreply.github.com>
2024-02-06 17:37:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,29 +27,40 @@ while game_is_on:
     screen.update()
     ball.move()
 
-    #Detect collision with wall
-
+    # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    #Detect collision with paddle
+    # Detect collision with paddle
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) and ball.heading() < 180:
+        if not ball.is_bouncing_paddle:
+            ball.is_bouncing_paddle = True
+            scoreboard.r_point()
+            ball.bounce_x()
+    elif (ball.distance(l_paddle) < 50 and ball.xcor() < -320) and ball.heading() > 180:
+        if not ball.is_bouncing_paddle:
+            ball.is_bouncing_paddle = True
+            scoreboard.l_point()
+            ball.bounce_x()
+    else:
+        ball.is_bouncing_paddle = False
 
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
-        scoreboard.r_point()
-        ball.bounce_x()
-    elif ball.distance(l_paddle) < 50 and ball.xcor() < -320:
-        scoreboard.l_point()
-        ball.bounce_x()
-
-    #Detect R paddle misses
-
+    # Detect R paddle misses
     if ball.xcor() > 380:
+        scoreboard.l_point()
         ball.reset_position()
 
-
-    #Detect L paddle misses:
-
+    # Detect L paddle misses
     if ball.xcor() < -380:
+        scoreboard.r_point()
         ball.reset_position()
+
+    # Check if the game should end
+    if scoreboard.l_score == 10:
+        print("Left player wins!")
+        game_is_on = False
+    elif scoreboard.r_score == 10:
+        print("Right player wins!")
+        game_is_on = False
 
 screen.exitonclick()
